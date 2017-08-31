2b74bb5c9a88a8bdcc50253a52c1e0e9c5bbbb03
Tim-Oliver Husser <mail@husser.de>
2017-08-31 18:44:50 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pytel-ascom',
+    version='0.1',
+    packages=['pytel_ascom'],
+    url='',
+    license='',
+    author='Tim-Oliver Husser',
+    author_email='husser@astro.physik.uni-goettingen.de',
+    description='Ascom wrapper for pytel',
+    requires=[]
+)
